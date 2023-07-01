c695bc232b323f23f00a628257fffc19c9c1e66f
Claudio Ramos de oliveira <claudior.oliveira@e-ai.com.br>
2023-07-01 18:47:05 -0300

    inicio da validacao de request

--- src/adapter/validation/validateRequest.ts
+++ src/adapter/validation/validateRequest.ts
@@ -1,9 +1,21 @@
 
-const validateRequest = ( request: Object) => {
+import { plainToClass, plainToInstance } from 'class-transformer';
 
+import { validate, ValidationError } from 'class-validator';
+
+const validateRequest = async ( classType: <T>, requestBody: any) => {
+
+
+        const object = plainToInstance(classType: classType.prototype, requestBody);
+        const errors: ValidationError[] = await validate(object);
     
-
-
+        if (errors.length > 0) {
+          const errorMessages: string[] = errors.map((error) => Object.values(error.constraints || "")).join(",").split(",");
+          return null;
+        }
+    
+        return object;
+      
 }
 
 
