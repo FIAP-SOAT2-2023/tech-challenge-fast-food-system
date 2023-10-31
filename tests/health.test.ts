086b23ac9949265bad3f3e86b369d879ffdf80d7
felipenatividade <felipecmro@hotmail.com>
2023-10-31 00:08:28 -0300

    create unit test

---
+++ tests/health.test.ts
@@ -0,0 +1,10 @@
+// health.test.ts
+
+function testFunction() {
+  return [];
+}
+
+test('testFunction should return an empty array', () => {
+  const result = testFunction();
+  expect(result).toEqual([]);
+});
